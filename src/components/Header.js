90f3e5a0f83d0aacef55788744bba424f7e715c0
Emma Price <ekprice01@gmail.com>
2019-08-07 18:21:10 +0100

    corrected link

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#contacts">Contact</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="slide-container ">
         <Slide {...properties}>
